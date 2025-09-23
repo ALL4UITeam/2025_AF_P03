3eca0f7bca66cadc9eb76ae892360cdf4ccc40cc
richosj <okm760596@gmail.com>
2025-09-23 18:10:12 +0900

    Updates

--- assets/js/index2.js
+++ assets/js/index2.js
@@ -1,16 +1,16 @@
 gsap.registerPlugin(ScrollTrigger, ScrollToPlugin, ScrollSmoother);
-ScrollSmoother.create({
+const smoother = ScrollSmoother.create({
   wrapper: "#smoother-wrapper",
   content: "#smoother-content",
   smooth: 2,
-  speed: 1,
-  normalizeScroll: true,
-  ignoreMobileResize: true,
-  smoothTouch: 0.1,
   effects: true,
-  preventDefault: true,
-  normalizeScroll: { allowNestedScroll: true }
+  normalizeScroll: false,
+  preventDefault: false
 });
+smoother.refresh = () => {
+};
+ScrollTrigger.refresh = () => {
+};
 const nav = document.querySelector("#nav");
 const toggleBtn = document.querySelector(".mobile-btn-menu");
 const depth1Buttons = document.querySelectorAll(".gnb-lv1 > a");
